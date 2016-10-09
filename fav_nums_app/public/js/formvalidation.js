448d9cb856895ef97e5f6e3e657c23a0e7fb6f91
JTong <eldertong@gmail.com>
2016-10-09 16:46:41 -0400

    initial commit

---
+++ fav_nums_app/public/js/formvalidation.js
@@ -0,0 +1,8 @@
+function validateForm() {
+    var value = document.forms["name_form"]["input_name"].value;
+    if (value.length < 3) {
+     document.getElementById("error").innerHTML = "Try Again but this time enter your name!";
+    }
+} 		
+
+												
